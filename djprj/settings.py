12731fc5454125b2c044cb710449af9047574ce7
Mykola Lev <mlev@disroot.org>
2019-02-10 13:49:37 +0200

    remove secret key

--- djprj/settings.py
+++ djprj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=1)rs6bjc+m!2#o8o!eooqln&s$j$my52t)fqele_+ngpo6--b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
