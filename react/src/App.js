a293032679c818513403ce47a8e1d031eb40b83d
vladyslav.semenchuk <vladyslav.semenchuk@globallogic.com>
2019-02-10 22:44:06 +0200

    MVP face recognizer

--- react/src/App.js
+++ react/src/App.js
@@ -11,29 +11,9 @@ import './App.css';
 
 class App extends Component {
     state = {
-        isLoading: true,
+        isLoading: false,
         isShowGallety: false,
         photos: []
-    }
-
-    componentDidMount() {
-        const tag = '',
-        apiKey = '4a2173dec440a23318af07a5354b082f',
-        albumId = 72157674388093532,
-        userId = '144522605@N06'
-
-        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&photoset_id=${albumId}&user_id=${userId}&tags=${tag}&format=json&nojsoncallback=1`)
-            .then(response => {
-                const photos = response.data.photos.photo,
-                    photoURLs = [];
-
-                photos.forEach(photo => {
-                    const photoURL = `http://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-                    photoURLs.push(photoURL);
-                });
-
-                this.setState({ isLoading: false, photos: photoURLs });
-            });
     }
 
     submitEmotions = emotions => {
@@ -45,7 +25,17 @@ class App extends Component {
 
         axios.get(proxyURL + url + `?emotions=${selectedEmotions}`)
             .then(response => {
-                this.setState({ isShowGallety: true, isLoading: false });
+                const photosURLs = [];
+
+                response.data.forEach(photoData => {
+                    photosURLs.push(photoData[1])
+                });
+
+                this.setState({
+                    isShowGallety: true,
+                    isLoading: false,
+                    photos: photosURLs
+                });
             });
     }
 
